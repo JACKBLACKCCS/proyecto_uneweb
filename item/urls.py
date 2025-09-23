25ea13c9181eb6ad98f1f1a55370b22abf17b10e
Alejandro <2020velasquez@gmail.com>
2025-09-23 18:31:59 -0400

    Clean up: remove old file locations after reorganization

--- item/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.item_list, name='item_list'),
-    path('create/', views.create_item, name='create_item'),  # <- aquí es donde carga su producto
-]
-
-# {% url 'item_list' %} → redirige a la lista de productos (tu “inicio” real del marketplace).
--- static/README.md
+++
@@ -1,2 +0,0 @@
-# Static files directory 
-Esta carpeta est  vac¡a porque no se utilizan archivos static en este proyecto. 
